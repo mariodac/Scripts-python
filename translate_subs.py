257387aa51e2f13d81cc802c649289cc76687eef
mariodac <mdac.mario@gmail.com>
2023-11-22 13:02:00 -0300

    Update translate_subs.py
    Adicionado pesquisa recursiva de arquivos em subpastas

--- translate_subs.py
+++ translate_subs.py
@@ -1,4 +1,8 @@
-import wx, os
+from os import listdir, path, system
+from re import search, IGNORECASE
+from wx import App, FileDialog, DirDialog, FD_OPEN, FD_FILE_MUST_EXIST, ID_OK, DD_DEFAULT_STYLE, DD_DIR_MUST_EXIST 
+
+total_files = []
 
 def wx_filedialog(wildcard):
     """Abre janela de dialogo para abrir arquivo
@@ -7,10 +11,10 @@ def wx_filedialog(wildcard):
     Returns:
         str: caminho do arquivo
         """
-    app = wx.App(None) # type: ignore
-    style = wx.FD_OPEN | wx.FD_FILE_MUST_EXIST
-    dialog = wx.FileDialog(None, 'Selecione o arquivo', wildcard=wildcard, style=style)
-    if dialog.ShowModal() == wx.ID_OK:
+    app = App(None) # type: ignore
+    style = FD_OPEN | FD_FILE_MUST_EXIST
+    dialog = FileDialog(None, 'Selecione o arquivo', wildcard=wildcard, style=style)
+    if dialog.ShowModal() == ID_OK:
         path = dialog.GetPath()
         dialog.Destroy()
         app.Destroy()
@@ -26,20 +30,54 @@ def wx_dirdialog():
     Returns:
         str: caminho da pasta
     """
-    app = wx.App(None)
-    dialog = wx.DirDialog (None, "Choose input directory", "", wx.DD_DEFAULT_STYLE | wx.DD_DIR_MUST_EXIST)
-    if dialog.ShowModal() == wx.ID_OK:
+    app = App(None)
+    dialog = DirDialog (None, "Choose input directory", "", DD_DEFAULT_STYLE | DD_DIR_MUST_EXIST)
+    if dialog.ShowModal() == ID_OK:
         path = dialog.GetPath()
     else:
         path = None
     dialog.Destroy()
     return path
 
+def listar_pasta(pasta):
+    tot = []
+    subpastas = list()
+    if path.isdir(pasta):
+        items = listdir(pasta)
+        for item in items:
+            if (not search('Recycle', item, IGNORECASE)) and (not search('System Volume Information', item, IGNORECASE)):
+                novo_item = path.join(pasta,item)
+                if path.isdir(novo_item):
+                    subpastas.append(novo_item)
+                    continue
+                if item.endswith('.mpg') or item.endswith('.mkv') or item.endswith('.mp4') or item.endswith('.avi') or item.endswith('.ass') or item.endswith('.srt'):
+                    tot.append(path.join(pasta, item))
+        for subpasta in subpastas:
+            if not search('Recycle', item, IGNORECASE):
+                tot.extend(listar_pasta(subpasta))
+    # arq.write("\n")
+    return tot
+
 if __name__ == '__main__':
     path_files = wx_dirdialog()
-    os.chdir(path_files)
-    files_path = [x for x in os.listdir(path_files) if x.endswith('.mkv') or x.endswith('.mkv') ]
-    for item in files_path:
-        new_item = item.split('.mkv')[0]
+    total_files.extend(listar_pasta(path_files))
+    # chdir(path_files)
+    # files_path = [x for x in listdir(path_files) if x.endswith('.mkv') or x.endswith('.mp4') or x.endswith('.avi') or x.endswith('.ass') or x.endswith('.srt')]
+    files_path = [path.join(path_files, x) for x in listdir(path_files) if x.endswith('.mkv') or x.endswith('.mpg') or x.endswith('.mp4') or x.endswith('.avi') or x.endswith('.ass') or x.endswith('.srt')]
+    for item in total_files:
+        if item.endswith('.mkv'):
+            new_item = item.split('.mkv')[0]
+        elif item.endswith('.mpg'):
+            new_item = item.split('.mpg')[0]
+        elif item.endswith('.mp4'):
+            new_item = item.split('.mp4')[0]
+        elif item.endswith('.avi'):
+            new_item = item.split('.avi')[0]
+        elif item.endswith('.ass'):
+            new_item = item.split('.ass')[0]
+        elif item.endswith('.srt'):
+            new_item = item.split('.srt')[0]
+        else:
+            print(f"Arquivo {item} com formato nao suportado")
         print(f"Gerando legenda para {new_item}")
-        os.system(f'translatesubs "{item}" "{new_item}_PT-BR.ass" --to_lang pt')
+        system(f'translatesubs "{item}" "{new_item}_PT-BR.ass" --to_lang pt')
