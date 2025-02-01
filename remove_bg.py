8eb77f8580138724ede348701f95c1625fd03991
mariodac <mdac.mario@gmail.com>
2025-01-31 21:08:10 -0300

    Update remove_bg.py
    atualizado root path

--- remove_bg.py
+++ remove_bg.py
@@ -1,9 +1,10 @@
 from rembg import remove
 from PIL import Image
 from pathlib import Path
-root_path = Path(__file__).parent
-input_path = 'teste.jpg'
-output_path = 'multiversus.png'
+from os import environ
+root_path = Path(environ['USERPROFILE']) / 'Downloads'
+input_path = 'input.png'
+output_path = 'output.png'
 inp = Image.open(root_path / input_path)
 output = remove(inp)
 output.save(root_path / output_path)
